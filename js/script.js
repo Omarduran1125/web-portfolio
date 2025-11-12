70c02a51b1db17f2f7a04054f99bb0fefd25ae6e
Omarduran1125 <oduran@student.scitech.edu>
2025-11-11 20:53:49 -0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,19 +3,19 @@ const form = document.querySelector("form");
 const submitButton = document.getElementById("submit-btn");
 const messageContainer = document.getElementById("message-container");
 
-// Listen for form submission
+// Listen for click
 submitButton.addEventListener("click", function (event) {
   event.preventDefault(); // stop page reload
 
   // Create the massage
   messageContainer.innerHTML = `
-    <p class="thankyou-msg">✅ Thank you for your massage! </p>
+    <p class="thankyou-msg">✅ Thank you for your massage!.</p>
   `;
 
-  // clear the form
+  // Optionally clear the form
   form.reset();
 
-  // Fade out the massage 
+  // Fade out the massage
   setTimeout(() => {
     messageContainer.innerHTML = "";
   }, 4000);
